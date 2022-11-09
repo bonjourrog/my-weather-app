66ed1fa0ef955461259c837093654b066b319bcf
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2022-11-08 18:12:42 -0700

    Create currentWeather component

---
+++ src/views/currentWeather/index.js
@@ -0,0 +1,14 @@
+import { useSelector } from 'react-redux'
+import './style.css'
+export default function CurrentWeather(){
+
+    const {current} = useSelector(state => state.weather);
+
+    return(
+        <div className="current-weather">
+            <h2 className="current-weather__condition">{current.condition.text}</h2>
+            <h2 className="current-weather__temp">{current.temp.c}°</h2>
+            <img className="current-weather__icon" src={current.condition.icon}></img>
+        </div>
+    )
+}
